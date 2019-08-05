f549e4cd5ec477f5b40753163c7b3bc69b496540
Sergi Marquez <sertrack8@gmail.com>
2019-08-05 09:44:23 +0200

    Added semantic ui and created the form and the event handlers.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    // so the browser doesn't refresh
+    // the whole page every time the user submits
+    event.preventDefault();
+
+    // TODO: Make sure we call
+    // callback from parent component
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="ui form">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
