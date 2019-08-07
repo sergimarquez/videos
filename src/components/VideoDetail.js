be918bed95febdc226ce7f88b39d1ccf40aa92e9
Sergi Marquez <sertrack8@gmail.com>
2019-08-07 08:35:45 +0200

    Fixed a few warnings and created the layout using semantic ui.

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -12,7 +12,7 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div className="ui embed">
-        <iframe src={videoSrc} />
+        <iframe title="videp player" src={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
